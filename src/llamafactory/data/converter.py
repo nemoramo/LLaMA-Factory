50c6c9bd75f35e947cb15756aaea3b36aab5a2ca
ramos.ma <ramos.ma@airudder.com>
2025-11-18 14:33:58 +0800

    Change media file warnings to debug level

--- src/llamafactory/data/converter.py
+++ src/llamafactory/data/converter.py
@@ -58,9 +58,7 @@ class DatasetConverter:
                     if os.path.isfile(media_path):
                         medias[i] = media_path
                     else:
-                        logger.warning_rank0_once(
-                            f"Media {medias[i]} does not exist in `media_dir`. Use original path."
-                        )
+                        logger.debug_rank0_once(f"Media {medias[i]} does not exist in `media_dir`. Use original path.")
             elif isinstance(medias[0], list):  # for processed video frames
                 # medias is a list of lists, e.g., [[frame1.jpg, frame2.jpg], [frame3.jpg, frame4.jpg]]
                 for i in range(len(medias)):
@@ -69,7 +67,7 @@ class DatasetConverter:
                         if os.path.isfile(media_path):
                             medias[i][j] = media_path
                         else:
-                            logger.warning_rank0_once(
+                            logger.debug_rank0_once(
                                 f"Media {medias[i][j]} does not exist in `media_dir`. Use original path."
                             )
 
